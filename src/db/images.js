d586f25b2453af75901ab540cd0825e52ef94f43
ravik003 <ravi31.ugec20@iiitranchi.ac.in>
2023-08-04 10:32:21 +0530

    initial commit

---
+++ src/db/images.js
@@ -0,0 +1,59 @@
+export const images = [
+    {
+        image: "https://wallpapercave.com/wp/wp12501451.jpg",
+    },
+    {
+        image: "https://wallpapercave.com/uwp/uwp3818245.jpeg",
+    },
+    {
+        image: "https://wallpapercave.com/uwp/uwp3820363.jpeg",
+    },
+    {
+        image: "https://wallpapercave.com/uwp/uwp3855765.jpeg",
+    },
+    {
+        image: "https://wallpapercave.com/dwp1x/wp12119170.jpg",
+    },
+    {
+        image: "https://wallpapercave.com/dwp1x/wp10864103.jpg",
+    },
+    {
+        image: "https://wallpapercave.com/uwp/uwp3534584.jpeg",
+    },
+    {
+        image: "https://wallpapercave.com/uwp/uwp3568055.webp",
+    },
+    {
+        image: "https://wallpapercave.com/uwp/uwp3813438.jpeg",
+    },
+    {
+        image: "https://wallpapercave.com/uwp/uwp3855758.jpeg",
+    },
+    {
+        image: "https://wallpapercave.com/dwp1x/wp12019890.jpg",
+    },
+    {
+        image: "https://wallpapercave.com/dwp1x/wp12019894.png",
+    },
+    {
+        image: "https://wallpapercave.com/dwp1x/wp8732582.jpg",
+    },
+    {
+        image: "https://wallpapercave.com/dwp1x/wp12019937.png",
+    },
+    {
+        image: "https://wallpapercave.com/uwp/uwp3481913.jpeg",
+    },
+    {
+        image: "https://wallpapercave.com/uwp/uwp3838384.webp",
+    },
+    {
+        image: "https://wallpapercave.com/uwp/uwp3838581.jpeg",
+    },
+    {
+        image: "https://images.pexels.com/photos/1933239/pexels-photo-1933239.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+    {
+        image: "https://images.pexels.com/photos/33545/sunrise-phu-quoc-island-ocean.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    }
+]
